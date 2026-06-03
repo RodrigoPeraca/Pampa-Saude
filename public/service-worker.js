61bd22cb2ef75d5d6462ecf7b3c29b0ec948cf1b
Peraça <rodrigoperaca.aluno@unipampa.edu.br>
2026-06-03 16:37:50 -0300

    remoção do serviceWorker apenas para o firebase

--- public/service-worker.js
+++ public/service-worker.js
@@ -87,12 +87,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_BZSRZFImnVwogdFvkdbufcIEKMjLYBY",
-  authDomain: "pampa-saude.firebaseapp.com",
-  projectId: "pampa-saude",
-  storageBucket: "pampa-saude.firebasestorage.app",
-  messagingSenderId: "403037160519",
-  appId: "1:403037160519:web:b072da322c05bf61631d62",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
